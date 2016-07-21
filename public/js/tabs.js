9478a916aae038791f80f78a914f492486fa625c
Ivan <Holy.vinil@gmail.com>
2016-07-21 22:09:58 +0300

    ss

---
+++ public/js/tabs.js
@@ -0,0 +1,12 @@
+(function ($) {
+    $(function () {
+
+        $('ul.tabs-list').on('click', 'li:not(.active)', function () {
+            $(this)
+                .addClass('active').siblings().removeClass('active')
+                .closest('section').find('ul.brands-list').removeClass('active').eq($(this).index()).addClass('active');
+        });
+
+    });
+})(jQuery);
+
