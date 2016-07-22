0e4286d68c7b25a0ee4bb1dd19920ee7d0664c3b
Ivan <Holy.vinil@gmail.com>
2016-07-22 16:02:17 +0300

    ss

--- public/js/tabs.js
+++ public/js/tabs.js
@@ -1,11 +1,35 @@
-(function ($) {
-    $(function () {
-        $('ul.tabs-list').on('click', 'li:not(.active)', function () {
-            $(this)
-                .addClass('active').siblings().removeClass('active')
-                .closest('section').find('ul.brands-list').removeClass('active').eq($(this).index()).addClass('active');
-        });
-
+$(document).ready(function () {
+    $('ul.tabs-list').on('click', 'li:not(.active)', function () {
+        $(this)
+            .addClass('active').siblings().removeClass('active')
+            .closest('section').find('ul.brands-list').removeClass('active').eq($(this).index()).addClass('active');
     });
-})(jQuery);
+
+/* $('ul.tabs-list li').on('click','li:(.active)',function () {
+     $(".active").toggle(".arrow-top");
+    });*/
+
+/*        $(".active").toggle(".arrow-top").css("display", "block");*/
+
+
+
+  /*  $("ul.tabs-list li").one("click", function () {
+        if ($(this).is(":first-child")) {
+            $(".arrow-top").css("display", "block")
+            $("p").text("It's the first div.");
+        } else if ($(this).is(".blue,.red")) {
+            $("p").text("It's a blue or red div.");
+        } else if ($(this).is(":contains('Peter')")) {
+            $("p").text("It's Peter!");
+        } else {
+            $("p").html("It's nothing <em>special</em>.");
+        }
+        $("p").hide().slideDown("slow");
+        $(this).css({
+            "border-style": "inset",
+            cursor: "default"
+        });
+    });*/
+});
+
 
